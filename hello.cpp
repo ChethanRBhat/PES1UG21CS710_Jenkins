f399cf39a3ed75759904a89289968f0076acfff1
Chethan R <vighneshvr2002@gmail.com>
2024-03-09 11:33:52 +0530

    Describe this commit

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+// File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout << "PES1UG21CS710\n";
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
